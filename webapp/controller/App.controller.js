237570e213d62cce83ae3121d5d6cb8560eb7e8c
Mayko Facci <mk0f@outlook.com>
2025-10-25 08:48:18 -0300

    Implementing the root view for a list-detail application

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,14 +1,19 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function(BaseController) {
-      "use strict";
-  
-      return BaseController.extend("student.com.sap.training.advancedsapui5.listdetail.controller.App", {
-        onInit: function() {
-        }
-      });
-    }
-  );
-  
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+  ],
+  function (BaseController, JSONModel) {
+    "use strict";
+
+    return BaseController.extend("student.com.sap.training.advancedsapui5.listdetail.controller.App", {
+      onInit: function () {
+        var oViewModel = new JSONModel({
+          layout: "OneColumn"
+        });
+
+        this.getView().setModel(oViewModel, "mainView");
+      }
+    });
+  }
+);
